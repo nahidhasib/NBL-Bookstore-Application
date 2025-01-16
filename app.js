fa45562ead25e8dc11db90f34fcdfb68e067f4bc
nahidhasib <hasib0miazi@gmail.com>
2025-01-16 12:00:31 +0530

    fixed some issues

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('./config/db');
+const app = express()
+const port = process.env.PORT || 8080;
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true 
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Replace with your RDS instance details
 const connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
+  host: process.env.RDS_HOSTNAME || "awseb-e-8yisfmhhni-stack-awsebrdsdatabase-9wm74wpcbiiz.c12umuoyandg.eu-west-2.rds.amazonaws.com",
+  user: process.env.RDS_USERNAME || "admin",
+  password: process.env.RDS_PASSWORD || "adminpass1234",
+  port: process.env.RDS_PORT || 3306,
+  database: process.env.RDS_DB_NAME || "awseb-e-8yisfmhhni-stack-awsebrdsdatabase-9wm74wpcbiiz",
 });
 
 connection.connect((err) => {
